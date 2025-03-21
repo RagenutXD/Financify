4ab675af0bb111bfbea5aecad74963108e430b61
RagenutXD <ragenut014@gmail.com>
2025-03-21 19:43:09 +0800

    added the previous files

---
+++ src/com/financify/statistics/JParse.java
@@ -0,0 +1,66 @@
+package com.financify.statistics;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class JParse {
+	
+	private JSONObject jsonObject;
+
+	public JParse(String file_dir) {
+		String jsonString = getJSONFromFile(file_dir);
+		
+		try {
+			
+			JSONParser parser = new JSONParser();
+			Object object = parser.parse(jsonString);
+			jsonObject = (JSONObject) object;
+			
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	
+	private String getJSONFromFile(String file_dir) {
+		String jsonText= "";
+		try {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(file_dir));
+			String line;
+			while((line = bufferedReader.readLine()) != null) {
+				jsonText += line +"\n";
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return jsonText;
+		
+	}
+	
+	public String getStringFromKey(String key) {
+		return (String) jsonObject.get(key);
+	}
+	
+	public JSONObject getObjectFromKey(String key) {
+		return (JSONObject) jsonObject.get(key);
+	}
+	
+	public int getIntFromKey(String key) {
+		return (int) jsonObject.get(key);
+	}
+	
+	public double getDoubleFromKey(String key) {
+		return (double) jsonObject.get(key);
+	}
+	
+	public JSONArray getArrayFromKey(String key) {
+		return (JSONArray) jsonObject.get(key);
+	}
+}
